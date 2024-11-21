4040f362db2240228f36dc497e3a75cb4c3f2b39
jogoCode <163865984+jogoCode@users.noreply.github.com>
2024-11-21 15:40:31 +0400

    start oscillator rotation

--- Assets/Scripts/FeedBack/Oscillator/OscillatorRotation.cs
+++ Assets/Scripts/FeedBack/Oscillator/OscillatorRotation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
-public class OscillatorScale : Oscillator
+public class OscillatorRotation : Oscillator
 {
     Vector3 m_baseTargetScale;
 
@@ -14,10 +14,6 @@ public class OscillatorScale : Oscillator
     public override void Update()
     {
         base.Update();
-        Vector3 newScale = m_baseTargetScale + new Vector3(m_displacement, -m_displacement) * 4;
-        newScale.x = Mathf.Clamp(newScale.x, 0, 1000);
-        newScale.y = Mathf.Clamp(newScale.y, 0, 1000);
-        newScale.z = Mathf.Clamp(newScale.z, 0, 1000);
-        m_target.transform.localScale = newScale;
+       
     }
 }
